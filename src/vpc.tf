1294f839757a78f98db4087f0ea99ea189163594
dixitvij43015 <137907007+dixitvij43015@users.noreply.github.com>
2025-04-16 00:03:53 -0400

    Update vpc.tf
    updated code woth RT subnet ig

--- src/vpc.tf
+++ src/vpc.tf
@@ -1,4 +1,11 @@
-
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.81.0"
+    }
+  }
+}
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
   instance_tenancy = "default"
@@ -8,4 +15,54 @@ resource "aws_vpc" "vpc" {
   tags = {
     Name = "terraformVPC"
   }
+}
+
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "tfigw"
+  }
+}
+
+resource "aws_subnet" "public-subnet"{
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    name = "tfPublicSubnet"
+  }
+}
+
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "10.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    name = "tfPublicRT"
+  }
+}
+
+
+resource "aws_route_table_association" "route-table-association" {
+  route_table_id = aws_route_table.public-route-table.id
+  subnet_id = aws_subnet.public-subnet.id
+}
+
+resource "aws_subnet" "private-subnet"{
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = false
+  availability_zone = "us-east-1a"
+
+  tags = {
+    name = "tfprivateSubnet"
+  }
+
 }
